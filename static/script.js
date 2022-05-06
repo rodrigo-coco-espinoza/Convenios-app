250df3bcde804df34cacb6a1111b6aa447b75c24
rodrigo-coco-espinoza <respinozafuentes@gmail.com>
2022-05-06 17:49:12 -0400

    Delete static directory

--- static/script.js
+++
@@ -1,7 +0,0 @@
-function changeActive(id){
-  const sideMenu = document.querySelectorAll("#sidebarMenu a");
-  sideMenu.forEach(option => {
-    option.classList.remove('active');
-  });
-  document.querySelector(id).classList.add('active');
-}
